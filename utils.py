535a5d7b51091d286a88925d4bbbf23b278a1d40
Zhang <hzhang@ro.wucon.wustl.edu>
2017-10-24 17:57:51 -0500

    Cleaned the transfer to tfrecord

--- utils.py
+++ utils.py
@@ -42,6 +42,9 @@ class Data(object):
         self.name = name
         self.shuffle = shuffle
         self.random_state = random_state
+        
+        if not os.path.exists(self.save_dir):
+            os.mkdir(self.save_dir)
     
     def get_files(self):
         """Get the list of input images and output images in pairs
@@ -63,11 +66,10 @@ class Data(object):
             assert (in_file.endswith('.png')), 'The image should be *.png'
             assert (out_file.endswith('.png')), 'The image should be *.png'
 
-            input_image_list.append(in_file)
-            output_image_list.append(out_file)
-
+            input_image_list.append(os.path.join(self.input_data_dir, in_file))
+            output_image_list.append(os.path.join(self.output_data_dir, out_file))
         if self.shuffle:
-            combined = zip(input_image_list, output_image_list)
+            combined = list(zip(input_image_list, output_image_list))
             prng = np.random.RandomState(self.random_state)
             prng.shuffle(combined)
             input_image_list, output_image_list = zip(*combined)
@@ -76,9 +78,6 @@ class Data(object):
     
     def _bytes_feature(self, value):
         return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
-
-    def _int64_feature(self, value):
-        return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
     def convert_to_tfrecord(self, input_image_list, output_image_list):
         """Convert all images pairs to tfrecord files and save as name
@@ -94,17 +93,19 @@ class Data(object):
         writer = tf.python_io.TFRecordWriter(filename)
         n_samples = len(input_image_list)
         for i in range(n_samples):
-            i_image = input_image_list[i]
-            o_image = output_image_list[i]
-            i_height, i_width = i_image.shape[:2]
-            o_height, o_width = o_image.shape[:2]
+            in_image = imread(input_image_list[i]).astype('int32')
+            out_image = imread(output_image_list[i]).astype('int32')
+            # If the image is in grayscale, resize to (h, w, 1)
+            if len(in_image.shape) < 3:
+                in_image = in_image.reshape(in_image.shape+(1,))
+                out_image = out_image.reshape(out_image.shape+(1,))
+            in_shape = np.array(in_image.shape, np.int32)
+            out_shape = np.array(out_image.shape, np.int32)
             sample = tf.train.Example(features=tf.train.Features(feature={
-                'i_height': self._int64_feature(i_height),
-                'i_width': self._int64_feature(i_width),
-                'o_height': self._int64_feature(o_height),
-                'o_width': self._int64_feature(o_width),
-                'i_image_raw': self._bytes_feature(i_image.tostring()),
-                'o_image_raw': self._bytes_feature(o_image.tostring())
+                'in_shape': self._bytes_feature(in_shape.tostring()),
+                'out_shape': self._bytes_feature(out_shape.tostring()),
+                'in_image_raw': self._bytes_feature(in_image.tostring()),
+                'out_image_raw': self._bytes_feature(out_image.tostring())
             }))
             writer.write(sample.SerializeToString())
         writer.close()
